c2d529212e1f06a4fafe08c6409b900cd30a9645
J Dark <james.dark.95@gmail.com>
2024-01-09 11:39:31 +0100

    better values

--- section_3_model_parameter_evaluation/generate_annealing_plot_data.py
+++ section_3_model_parameter_evaluation/generate_annealing_plot_data.py
@@ -19,8 +19,8 @@ t_annealing = np.linspace(0, annealing_time, num=1000)
 T_values = np.linspace(1, 800, num=1000)
 
 A_0_1 = 6.1834e-03
-E_A_1 = 0.25
-E_A_2 = 0.33
+E_A_1 = 0.24
+E_A_2 = 0.30
 
 # dummy values for no annealing
 A_0_2 = 0
@@ -31,29 +31,46 @@ phi = 0
 K = 1
 n_max = 1
 
-n_0_defect_1, n_0_defect_2, n_0_defect_3 = defect_type_1_densities[0], defect_type_2_densities[0], defect_type_3_densities[0]
+n_0_defect_1, n_0_defect_2, n_0_defect_3 = (
+    defect_type_1_densities[0],
+    defect_type_2_densities[0],
+    defect_type_3_densities[0],
+)
 
-annealed_defect_1_densities, annealed_defect_2_densities, annealed_defect_3_densities = [], [], []
+(
+    annealed_defect_1_densities,
+    annealed_defect_2_densities,
+    annealed_defect_3_densities,
+) = ([], [], [])
 
 for T in T_values:
     # defect type 1
     defect_1_extra_args = (phi, K, n_max, A_0_1, E_A_1, T)
     n_traps_annleaing_defect_1 = odeint(
-        neutron_trap_creation_numerical, n_0_defect_1, t_annealing, args=defect_1_extra_args
+        neutron_trap_creation_numerical,
+        n_0_defect_1,
+        t_annealing,
+        args=defect_1_extra_args,
     )
     annealed_defect_1_densities.append(n_traps_annleaing_defect_1[-1])
 
     # defect type 2
     defect_2_extra_args = (phi, K, n_max, A_0_1, E_A_2, T)
     n_traps_annleaing_defect_2 = odeint(
-        neutron_trap_creation_numerical, n_0_defect_2, t_annealing, args=defect_2_extra_args
+        neutron_trap_creation_numerical,
+        n_0_defect_2,
+        t_annealing,
+        args=defect_2_extra_args,
     )
     annealed_defect_2_densities.append(n_traps_annleaing_defect_2[-1])
-    
+
     # defect type 3
     defect_3_extra_args = (phi, K, n_max, A_0_2, E_A_3, T)
     n_traps_annleaing_defect_3 = odeint(
-        neutron_trap_creation_numerical, n_0_defect_3, t_annealing, args=defect_3_extra_args
+        neutron_trap_creation_numerical,
+        n_0_defect_3,
+        t_annealing,
+        args=defect_3_extra_args,
     )
     annealed_defect_3_densities.append(n_traps_annleaing_defect_3[-1])
 
