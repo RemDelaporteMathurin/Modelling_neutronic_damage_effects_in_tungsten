4ee7de5b326372588419af59fd7f8c810ac0cf58
James laptop <james.dark.95@gmail.com>
2024-02-15 11:12:12 +0100

    remove duplication

--- section_4_impact_on_trap_concentration_and_tritium_inventories/generate_data.py
+++ section_4_impact_on_trap_concentration_and_tritium_inventories/generate_data.py
@@ -1,33 +1,5 @@
 from festim_model import festim_sim
-import festim as F
 import numpy as np
-from scipy.integrate import odeint
-from os.path import exists
-
-# defect type I
-A_0_1 = 6.1838e-03
-E_A_1 = 0.24
-
-trap_D1_K = 9.0e26
-trap_D1_n_max = 6.9e25
-trap_D2_K = 4.2e26
-trap_D2_n_max = 7.0e25
-
-# defect type II
-A_0_2 = 6.1838e-03
-E_A_2 = 0.30
-
-trap_D3_K = 2.5e26
-trap_D3_n_max = 6.0e25
-trap_D4_K = 5.0e26
-trap_D4_n_max = 4.7e25
-
-# defect type III
-A_0_3 = 0
-E_A_3 = 1
-
-trap_D5_K = 1.0e26
-trap_D5_n_max = 2.0e25
 
 # common values
 k_B = F.k_B  # eV K-1
